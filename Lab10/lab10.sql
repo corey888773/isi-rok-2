9f7f32ad46575c87e0060bccde08dfce4b5a0c17
Gasiorek, Piotr <piotrgasiorek773@gmail.com>
2023-01-21 23:16:31 +0100

    nauka do Kolosa, Lab10

--- Lab10/lab10.sql
+++ Lab10/lab10.sql
@@ -1,13 +1,10 @@
--- 1
+set search_path to public;
 
+--1
 SELECT DISTINCT nazwa
 FROM pudelka NATURAL JOIN zawartosc
 WHERE idczekoladki
   IN (SELECT idczekoladki FROM czekoladki ORDER BY koszt LIMIT 3);
-
-SELECT nazwa, koszt
-FROM czekoladki
-WHERE koszt = (SELECT MAX(koszt) FROM czekoladki);
 
 SELECT p.nazwa, idpudelka
 FROM (SELECT idczekoladki FROM czekoladki ORDER BY koszt LIMIT 3)
@@ -15,42 +12,74 @@ FROM (SELECT idczekoladki FROM czekoladki ORDER BY koszt LIMIT 3)
 NATURAL JOIN zawartosc
 NATURAL JOIN pudelka p;
 
+SELECT nazwa
+FROM czekoladki
+WHERE koszt = (SELECT MAX(koszt) FROM czekoladki);
+
 SELECT nazwa, koszt, (SELECT MAX(koszt) FROM czekoladki) AS MAX FROM czekoladki;
 
--- 2
-select datarealizacji, idzamowienia from zamowienia where idklienta in (select idklienta from klienci where nazwa LIKE '%Antoni%');
-select datarealizacji, idzamowienia from zamowienia where idklienta in (select idklienta from klienci where ulica LIKE '%/%');
-select datarealizacji, idzamowienia from zamowienia where idklienta in (select idklienta from klienci where miejscowosc LIKE '_rak_w') and extract(month from datarealizacji) = 11;
+ --2
+ SELECT * FROM zamowienia WHERE idklienta IN
+                                (SELECT idklienta FROM klienci WHERE nazwa LIKE '%ntoni%');
+
+SELECT * FROM zamowienia WHERE idklienta IN (SELECT idklienta FROM klienci WHERE klienci.ulica LIKE '%/%');
 
 -- 3
-select nazwa, ulica, miejscowosc from klienci where idklienta in (select idklienta from zamowienia where datarealizacji = '2013-11-12');
-select nazwa, ulica, miejscowosc from klienci where idklienta in (select idklienta from zamowienia where extract(month from datarealizacji) = 11 and extract(year from datarealizacji) = 2013);
-select nazwa, ulica, miejscowosc from klienci where idklienta in
-                                                    (select idklienta from zamowienia za join artykuly a on za.idzamowienia = a.idzamowienia join pudelka p on a.idpudelka = p.idpudelka
-                                                                      where p.nazwa in ('Kremowa fantazja' , 'Kolekcja jesienna'));
-select nazwa, ulica, miejscowosc from klienci where idklienta in
-                                                    (select idklienta from zamowienia za join artykuly a on za.idzamowienia = a.idzamowienia join pudelka p on a.idpudelka = p.idpudelka
-                                                                      where p.nazwa in ('Kremowa fantazja' , 'Kolekcja jesienna') and a.sztuk >= 2);
+SELECT * FROM klienci WHERE idklienta IN (SELECT idklienta FROM zamowienia WHERE datarealizacji ='12.11.2013'::date);
 
-select nazwa, ulica, miejscowosc from klienci where idklienta in
-                                                    (select idklienta from zamowienia za join artykuly a on za.idzamowienia = a.idzamowienia join pudelka p on a.idpudelka = p.idpudelka join zawartosc z on p.idpudelka = z.idpudelka join czekoladki c on z.idczekoladki = c.idczekoladki
-                                                                      where c.orzechy = 'migdały');
+SELECT * FROM klienci WHERE idklienta IN
+    (SELECT idklienta FROM zamowienia WHERE idzamowienia IN
+            (SELECT idzamowienia FROM artykuly WHERE idpudelka IN
+                    (SELECT idpudelka FROM pudelka WHERE nazwa IN ('Kremowa fantazja','Kolekcja jesienna'))));
 
-select nazwa, ulica, miejscowosc from klienci where idklienta in ( select idklienta from zamowienia);
-select nazwa, ulica, miejscowosc from klienci where idklienta not in ( select idklienta from zamowienia);
+SELECT * FROM klienci WHERE idklienta IN
+    (SELECT idklienta FROM zamowienia WHERE idzamowienia IN
+            (SELECT idzamowienia FROM artykuly WHERE sztuk >= 2 AND idpudelka IN
+                    (SELECT idpudelka FROM pudelka WHERE nazwa IN ('Kremowa fantazja','Kolekcja jesienna'))));
+select * from klienci where idklienta in
+    (select idklienta from zamowienia za join artykuly a on za.idzamowienia = a.idzamowienia join pudelka p on a.idpudelka = p.idpudelka
+        where p.nazwa in ('Kremowa fantazja' , 'Kolekcja jesienna') and a.sztuk >= 2);
+
+SELECT * FROM klienci WHERE idklienta NOT IN (SELECT idklienta FROM zamowienia);
 
 -- 4
-select nazwa, opis, cena from pudelka where idpudelka in (select p.idpudelka from pudelka p join zawartosc z on p.idpudelka = z.idpudelka join czekoladki c on z.idczekoladki = c.idczekoladki where c.idczekoladki = 'd09');
-select nazwa, opis, cena from pudelka where idpudelka in (select p.idpudelka from pudelka p join zawartosc z on p.idpudelka = z.idpudelka join czekoladki c on z.idczekoladki = c.idczekoladki where c.nazwa = 'Gorzka truskawkowa');
-select nazwa, opis, cena from pudelka where idpudelka in (select p.idpudelka from pudelka p join zawartosc z on p.idpudelka = z.idpudelka join czekoladki c on z.idczekoladki = c.idczekoladki where c.nazwa like 'S%' and z.sztuk >= 1);
-select nazwa, opis, cena from pudelka where idpudelka in (select p.idpudelka from pudelka p join zawartosc z on p.idpudelka = z.idpudelka join czekoladki c on z.idczekoladki = c.idczekoladki where z.sztuk >= 4);
---4.5
---4.6
-select nazwa, opis, cena from pudelka where idpudelka not in (select p.idpudelka from pudelka p join zawartosc z on p.idpudelka = z.idpudelka join czekoladki c on z.idczekoladki = c.idczekoladki where c.czekolada = 'gorzka');
---4.8
-select nazwa, opis, cena from pudelka where idpudelka in (select p.idpudelka from pudelka p join zawartosc z on p.idpudelka = z.idpudelka join czekoladki c on z.idczekoladki = c.idczekoladki where c.nadzienie is null);
+SELECT * FROM pudelka WHERE idpudelka IN
+  (SELECT idpudelka FROM zawartosc WHERE idczekoladki NOT IN
+     (SELECT idczekoladki FROM czekoladki WHERE orzechy IS NOT NULL));
 
--- 5
-select * from czekoladki
+SELECT * FROM pudelka WHERE idpudelka IN
+  (SELECT idpudelka FROM zawartosc z WHERE EXISTS
+     (SELECT idczekoladki FROM czekoladki c WHERE nadzienie IS NULL AND z.idczekoladki = c.idczekoladki));
 
+--5
+-- Kto (nazwa klienta) złożył zamówienia na takie same czekoladki (pudełka) jak zamawiała Gorka Alicja.
+SELECT * FROM klienci WHERE idklienta IN
+    (SELECT idklienta FROM zamowienia WHERE idzamowienia IN
+        (SELECT idzamowienia FROM artykuly WHERE idpudelka IN
+            (SELECT idpudelka FROM artykuly WHERE idzamowienia IN
+                (SELECT idzamowienia FROM zamowienia WHERE idklienta =
+                   (SELECT idklienta FROM klienci WHERE nazwa = 'Górka Alicja')))));
 
+SELECT * FROM klienci WHERE idklienta IN
+    (SELECT idklienta FROM zamowienia WHERE idzamowienia IN
+        (SELECT idzamowienia FROM artykuly WHERE idpudelka IN
+            (SELECT idpudelka FROM artykuly WHERE idzamowienia IN
+                (SELECT idzamowienia FROM zamowienia WHERE idklienta IN
+                   (SELECT idklienta FROM klienci WHERE miejscowosc ~* 'katowice')))));
+
+-- 6
+SELECT nazwa FROM pudelka WHERE idpudelka IN
+    (SELECT idpudelka FROM
+        (SELECT idpudelka, SUM(sztuk) as sztuk FROM zawartosc GROUP BY idpudelka) as id_sztuki
+            WHERE id_sztuki.sztuk = (SELECT MAX(x) FROM (SELECT SUM(sztuk) as x FROM zawartosc GROUP BY idpudelka) AS sum));
+
+SELECT nazwa FROM pudelka WHERE idpudelka IN
+    (SELECT idpudelka FROM
+        (SELECT idpudelka, SUM(sztuk) as sztuk FROM zawartosc GROUP BY idpudelka) as id_sztuki
+            WHERE id_sztuki.sztuk > (SELECT AVG(x) FROM (SELECT SUM(sztuk) as x FROM zawartosc GROUP BY idpudelka) AS sum));
+
+SELECT nazwa FROM pudelka WHERE idpudelka IN
+    (SELECT idpudelka FROM
+        (SELECT idpudelka, SUM(sztuk) as sztuk FROM zawartosc GROUP BY idpudelka) as id_sztuki
+            WHERE id_sztuki.sztuk = (SELECT MAX(x) FROM (SELECT SUM(sztuk) as x FROM zawartosc GROUP BY idpudelka) AS sum)
+               OR id_sztuki.sztuk = (SELECT MIN(x) FROM (SELECT SUM(sztuk) as x FROM zawartosc GROUP BY idpudelka) AS sum));
